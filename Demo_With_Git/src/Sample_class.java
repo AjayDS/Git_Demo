8d3956ab408147b70f6c6b276ff97e831d815a42
inasatish <inasatish@HIBAMP51284.ad.harman.com>
2016-12-06 12:28:15 +0530

    committing new class

---
+++ Demo_With_Git/src/Sample_class.java
@@ -0,0 +1,12 @@
+
+public class Sample_class {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.print("123123123");
+	}
+
+}
