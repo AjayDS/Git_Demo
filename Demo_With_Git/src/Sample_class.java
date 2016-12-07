3f6103fb7b3ba4b6d7f6af7384faf17a943750f3
inasatish <inasatish@HIBAMP51284.ad.harman.com>
2016-12-07 14:24:11 +0530

    Master copy push

--- Demo_With_Git/src/Sample_class.java
+++ Demo_With_Git/src/Sample_class.java
@@ -6,7 +6,7 @@ public class Sample_class {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.print("Developer comments");
+			System.out.print("Developer comments"); 
 	}
 
 }
